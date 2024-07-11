04f4e7015f9eaafbe815caebec96b4828058c07f
Aryo Prakarsa <30886148+aryoprakarsa@users.noreply.github.com>
2024-07-11 17:19:02 +0700

    update no build

--- src/components/SpeedTest.js
+++ src/components/SpeedTest.js
@@ -1,9 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { ProgressBar, Row, Col, Button, Container, Card, Alert } from 'react-bootstrap';
-import { Helmet } from 'react-helmet-async';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './SpeedTestResults.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  ProgressBar,
+  Row,
+  Col,
+  Button,
+  Container,
+  Card,
+  Alert,
+} from "react-bootstrap";
+import { Helmet } from "react-helmet-async";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./SpeedTestResults.css";
 
 const bytesToReadableSpeed = (bytes) => {
   if (bytes < 1024) {
@@ -31,17 +39,17 @@ const SpeedTest = () => {
 
   const fetchIpInfo = async () => {
     try {
-      const ipResponse = await axios.get('https://api.ipify.org?format=json');
+      const ipResponse = await axios.get("https://api.ipify.org?format=json");
       const ip = ipResponse.data.ip;
 
       const response = await axios.get(`https://ipapi.co/${ip}/json/`);
       setIpInfo(response.data);
     } catch (error) {
-      console.error('Error fetching IP info:', error);
+      console.error("Error fetching IP info:", error);
     }
   };
 
-  const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
   const testSpeed = async () => {
     setDownloadSpeed(null);
@@ -56,8 +64,10 @@ const SpeedTest = () => {
     setDelayMessage("");
 
     await testUploadSpeed();
-    setDelayMessage("Tests are completed. Please wait 5 seconds before starting a new test.");
-    await delay(5000); // 5-second delay after tests are complete
+    setDelayMessage(
+      "Tests are completed. Please wait 10 seconds before starting a new test."
+    );
+    await delay(10000); // 10-second delay after tests are complete
 
     setIsTesting(false);
     setDelayMessage("");
@@ -68,39 +78,44 @@ const SpeedTest = () => {
     const fileSizeInBytes = 10000000; // 10MB file size
 
     try {
-      await axios.get('https://www.speedtest.aryo.ai/test-file/10MB.bin', {
-        responseType: 'arraybuffer',
+      await axios.get("https://www.speedtest.aryo.ai/test-file/10MB.bin", {
+        responseType: "arraybuffer",
         onDownloadProgress: (progressEvent) => {
           const loaded = progressEvent.loaded;
           const total = progressEvent.total || fileSizeInBytes;
-          const percentCompleted = Math.min(Math.round((loaded * 100) / total), 100);
+          const percentCompleted = Math.min(
+            Math.round((loaded * 100) / total),
+            100
+          );
           setProgress(percentCompleted);
-        }
+        },
       });
       const endTime = new Date().getTime();
       const durationInSeconds = (endTime - startTime) / 1000;
       const speedInBps = (fileSizeInBytes * 8) / durationInSeconds;
       setDownloadSpeed(bytesToReadableSpeed(speedInBps));
     } catch (error) {
-      console.error('Error downloading the file:', error);
+      console.error("Error downloading the file:", error);
     }
   };
 
   const testUploadSpeed = async () => {
     const startTime = new Date().getTime();
     const fileSizeInBytes = 10000000; // 10MB file size
-    const testFile = new Blob([new Uint8Array(fileSizeInBytes)], { type: 'application/octet-stream' });
+    const testFile = new Blob([new Uint8Array(fileSizeInBytes)], {
+      type: "application/octet-stream",
+    });
 
     try {
       // Simulate initial connection setup delay
       await delay(100);
-      
+
       // Simulate a real network upload with varying delay
       for (let i = 0; i <= 100; i++) {
         setProgress(i);
         // Simulate variable network delay
-        const variableDelay = Math.random() * 10 + 20; // Random delay between 200ms and 500ms
-        await new Promise(resolve => setTimeout(resolve, variableDelay));
+        const variableDelay = Math.random() * 10 + 20; // Random delay
+        await new Promise((resolve) => setTimeout(resolve, variableDelay));
       }
 
       const endTime = new Date().getTime();
@@ -108,7 +123,7 @@ const SpeedTest = () => {
       const speedInBps = (fileSizeInBytes * 8) / durationInSeconds;
       setUploadSpeed(bytesToReadableSpeed(speedInBps));
     } catch (error) {
-      console.error('Error uploading the file:', error);
+      console.error("Error uploading the file:", error);
     }
   };
 
@@ -116,42 +131,63 @@ const SpeedTest = () => {
     <Container className="mt-4">
       <Helmet>
         <title>Speed Test Application</title>
-        <meta name="description" content="Test your download and upload speeds with our Speed Test Application." />
+        <meta
+          name="description"
+          content="Test your download and upload speeds with our Speed Test Application."
+        />
       </Helmet>
       <Row className="justify-content-center">
         <Col xs={12} md={8} lg={12}>
           <Card>
-            <Card.Header as="h1" className="text-center">Internet Speed Test</Card.Header>
+            <Card.Header as="h1" className="text-center">
+              Internet Speed Test
+            </Card.Header>
             <Card.Body className="text-center">
               {ipInfo.ip && (
                 <Alert variant="primary" className="mb-4">
-                  <strong>IP Address:</strong> {ipInfo.ip}<br />
-                  <strong>ISP:</strong> {ipInfo.org}<br />
+                  <strong>IP Address:</strong> {ipInfo.ip}
+                  <br />
+                  <strong>ISP:</strong> {ipInfo.org}
+                  <br />
                   <strong>ASN:</strong> {ipInfo.asn}
                 </Alert>
               )}
-              <Button 
-                className="mb-3 circle-button mx-auto" 
-                onClick={testSpeed} 
+              <Button
+                className="mb-3 circle-button mx-auto"
+                onClick={testSpeed}
                 disabled={isTesting}
               >
                 Start!
               </Button>
               {isTesting && (
                 <div className="progress-bar-container custom-progress-bar">
-                  <ProgressBar now={progress} label={`${progress}%`} style={{ height: '30px' }} />
+                  <ProgressBar
+                    now={progress}
+                    label={`${progress}%`}
+                    style={{ height: "30px" }}
+                  />
                 </div>
               )}
-              {downloadSpeed && <p className="bold-text">Download Speed: {downloadSpeed}</p>}
-              {uploadSpeed && <p className="bold-text">Upload Speed: {uploadSpeed}</p>}
+              {downloadSpeed && (
+                <p className="bold-text">Download Speed: {downloadSpeed}</p>
+              )}
+              {uploadSpeed && (
+                <p className="bold-text">Upload Speed: {uploadSpeed}</p>
+              )}
               {delayMessage && <p>{delayMessage}</p>}
             </Card.Body>
             <Card.Footer className="custom-card-footer">
               <strong>Note:</strong>
               <ol className="custom-list">
                 <li>The test server uses GitHub servers.</li>
-                <li>The download speed is calculated by downloading a 10MB file, and the upload speed is calculated by uploading a 10MB file.</li>
-                <li>There is a delay of 5 seconds before starting a new test to ensure optimal results.</li>
+                <li>
+                  The download speed is calculated by downloading a 10MB file,
+                  and the upload speed is calculated by uploading a 10MB file.
+                </li>
+                <li>
+                  There is a delay of 10 seconds before starting a new test to
+                  ensure optimal results.
+                </li>
               </ol>
             </Card.Footer>
           </Card>
